619e5a14165ec6b9a46134a44b2d53b3bfeaca31
EunKyung Shin <heybrilliante@gmail.com>
2021-05-25 11:17:45 +0900

    ch02 생성 및 수정

--- Myproject/src/ch02/Member.java
+++ Myproject/src/ch02/Member.java
@@ -1,11 +1,8 @@
 package ch02;
 
 public class Member {
-
-	int num; // 인스턴스 변수
-
 	public static void main(String[] args) {
-		System.out.println("안녕하세요 신은경입니다.");
+		int num; // 인스턴스 변수
 
 		// 변수선언
 		// 데이터 타입 식별 이름
@@ -20,18 +17,17 @@ public class Member {
 
 		// ③ double 타입의 키를 저장할 수 있는 변수 height를 정의해봅시다.
 		double height = 161.2d;
-		
+
 		// ④ boolean 타입의 JAVA책의 보유 여부를 저장할 수 있는 변수 hasBook를 정의해봅시다.
 		boolean hasBook = false;
-		
-		
+
 		// ⑤ 이름과 나이, 키, 책의 보유 여부를 출력해봅시다.
-		System.out.println("저의 이름은 "+name); 
-		System.out.println("키는 "+ height + "cm 입니다.");
-		System.out.println("제 나이는 "+ age +"살 입니다.");
-		System.out.println("책의 보유 여부 : " + hasBook	); //String + boolean
-	
-		if(hasBook) {
+		System.out.println("저의 이름은 " + name);
+		System.out.println("키는 " + height + "cm 입니다.");
+		System.out.println("제 나이는 " + age + "살 입니다.");
+		System.out.println("책의 보유 여부 : " + hasBook); // String + boolean
+
+		if (hasBook) {
 			System.out.println("책 있어요!");
 		} else {
 			System.out.println("책 없어요!");
