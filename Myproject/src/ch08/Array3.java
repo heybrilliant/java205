6e51b8f389835f54b24d38efe3a380382ef45fc0
EunKyung Shin <heybrilliante@gmail.com>
2021-05-28 00:37:33 +0900

    Update Array3.java
    수정

--- Myproject/src/ch08/Array3.java
+++ Myproject/src/ch08/Array3.java
@@ -3,31 +3,32 @@ package ch08;
 public class Array3 {
 
 	public static void renew(int[][] arr) {
-
+		
+		int[][] newArr = new int[arr.length][arr[0].length];
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[i].length; j++) {
-				System.out.print(arr[i][j]);
-				System.out.println();
+				if (i == arr.length-1) {
+					newArr[0][j] = arr[i][j];
+				} else {
+					newArr[i+1][j] = arr[i][j];
+				}
 			}
-//			int temp = (int) (arr.length);
-//			for (temp = i - 1; temp > 0; temp--) {
-//				if (temp == 1) {
-//					arr[temp] = arr[temp - 1];
-//					arr[0] = arr[temp];
-//				}
-//
-//				break;
-//
-//			}
+		}
+		
+		for (int i = 0; i < newArr.length; i++) {
+			for (int j = 0; j < newArr[i].length; j++) {
+				System.out.print(newArr[i][j] + " ");
+			}
+			System.out.println();
 		}
 	}
 
 	public static void main(String[] args) {
 		int[][] arr2 = new int[3][];
 
-		arr2[0] = new int[] { 1, 4, 5 };
-		arr2[1] = new int[] { 3, 1 };
-		arr2[2] = new int[] { 6, 2, 3 };
+		arr2[0] = new int[] { 1, 2, 3, 10};
+		arr2[1] = new int[] { 4, 5, 6, 11};
+		arr2[2] = new int[] { 7, 8, 9, 12};
 		renew(arr2);
 		System.out.println();
 	}
