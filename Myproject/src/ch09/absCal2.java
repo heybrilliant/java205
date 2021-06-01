49587a0bef0ac5a0e20eec3e0ec93c5e3c08cb61
EunKyung Shin <heybrilliante@gmail.com>
2021-06-01 16:52:55 +0900

    Update absCal2.java
    수정

--- Myproject/src/ch09/absCal2.java
+++ Myproject/src/ch09/absCal2.java
@@ -1,16 +1,16 @@
 package ch09;
 
-public class absCal2 extends absCal implements Calculator {
-	public static void main(String[] args) {
-		CalResult cr = new CalResult();
-		long n1 = 0;
-		long n2 = 0;
-		
-		cr.add(n1, n2);
-		cr.substract(n1, n2);
-		cr.multiply(n1, n2);
-		cr.divide(n1, n2);
-	}
+public class absCal2 implements Calculator {
+//	public static void main(String[] args) {
+//		CalResult cr = new CalResult();
+//		long n1 = 0;
+//		long n2 = 0;
+//		
+//		cr.add(n1, n2);
+//		cr.substract(n1, n2);
+//		cr.multiply(n1, n2);
+//		cr.divide(n1, n2);
+//	}
 
 	@Override
 	public long add(long n1, long n2) {
