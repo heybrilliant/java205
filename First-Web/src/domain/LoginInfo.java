44a56eb8ad7580aa493d6287d64ef95d60f889e5
EunKyung Shin <heybrilliante@gmail.com>
2021-07-24 23:30:53 +0900

    Update LoginInfo.java

--- First-Web/src/domain/LoginInfo.java
+++ First-Web/src/domain/LoginInfo.java
@@ -6,4 +6,31 @@ public class LoginInfo {
 	private String memberId;
 	private String memberName;
 	
+	
+	
+	public LoginInfo(int idx, String memberId, String memberName) {
+		this.idx = idx;
+		this.memberId = memberId;
+		this.memberName = memberName;
+	}
+
+	public int getIdx() {
+		return idx;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public String getMemberName() {
+		return memberName;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginInfo [idx=" + idx + ", memberId=" + memberId + ", memberName=" + memberName + "]";
+	}
+	
+	
+
 }
