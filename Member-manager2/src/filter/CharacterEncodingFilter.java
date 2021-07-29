7a8336052ba4d77ac6ac63219694e5ba47c918d1
EunKyung Shin <heybrilliante@gmail.com>
2021-07-29 23:47:55 +0900

    210729 회원관리 프로그램(member-manager2) 복습_filter

---
+++ Member-manager2/src/filter/CharacterEncodingFilter.java
@@ -0,0 +1,60 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+import org.omg.CORBA.PRIVATE_MEMBER;
+
+/**
+ * Servlet Filter implementation class CharacterEncodingFilter
+ */
+@WebFilter(dispatcherTypes = {DispatcherType.REQUEST }
+					, urlPatterns = { 
+							"/*",
+							"/request/*"
+							}, servletNames = { "Loader" })
+public class CharacterEncodingFilter implements Filter {
+	
+	private String encoding;
+
+    /**
+     * Default constructor. 
+     */
+    public CharacterEncodingFilter() {
+    	String encoding;
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest HttpRequest = (HttpServletRequest) request;
+		HttpRequest.setCharacterEncoding("utf-8");
+		
+		chain.doFilter(request, response);
+		
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		String paramValue = fConfig.getInitParameter("encoding");
+		if(paramValue != null) {
+			this.encoding = "utf-8";
+		} else {
+			this.encoding = paramValue;
+		}
+	}
+
+}
