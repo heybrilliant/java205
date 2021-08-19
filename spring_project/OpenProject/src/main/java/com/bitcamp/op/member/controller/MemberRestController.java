02931527bc0a783b0bd569ec8eda8638dfe6b4f5
EunKyung Shin <heybrilliante@gmail.com>
2021-08-19 18:27:49 +0900

    210819 Spring - update : add @CrossOrigin

--- spring_project/OpenProject/src/main/java/com/bitcamp/op/member/controller/MemberRestController.java
+++ spring_project/OpenProject/src/main/java/com/bitcamp/op/member/controller/MemberRestController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,6 +49,7 @@ public class MemberRestController {
 	// @PutMapping
 	// @PostMapping
 	@GetMapping("/members")
+	@CrossOrigin  // 요청이 어디서 들어와도 상관없음
 	public List<Member> getMembers(){
 		return restService.getMembers();
 	}
